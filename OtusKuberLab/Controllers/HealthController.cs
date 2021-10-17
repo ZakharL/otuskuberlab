7e43b239afeb09664ca4d71c2bc6b6240ccf9fff
Zakhar Luchinin <zakhar.luchinin@travelline.ru>
2021-10-17 21:57:32 +0300

    Initial commit

---
+++ OtusKuberLab/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtusKuberLab.Controllers
+{
+    [ApiController]
+    [Route( "[controller]" )]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "{\"status\": \"OK\"}";
+        }
+    }
+}
